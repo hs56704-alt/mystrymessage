83524ccf1144a76912b6a8789118b303919ced35
Himanshu Singh <hs56704@gmail.com>
2026-03-23 15:13:10 +0530

    23/03/2026 03.12

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+'use client';
 
-function page(){
-    return(
-        <div>page</div>
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <div>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
     )
+  }
+  return (
+    <div>
+      Not signed in <br/>
+      <button className= "bg-orange-500 px-3 py-1 m-4 rounded-2xl" onClick={() => signIn()}>Sign in</button>
+    </div>
+  )
 }
-
-export default page
