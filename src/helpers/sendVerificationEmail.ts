e599c8fc951577e2868964bfdeda6c4d02a0f3d7
Himanshu Singh <hs56704@gmail.com>
2026-03-23 18:14:34 +0530

    23/03/2026 06.15

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -10,7 +10,7 @@ export async function sendVerificationEmail(
     try{
         await resend.emails.send({
             from : "onboarding@resend.dev",
-            to : email ,
+            to : "chaudharybhai633@gmail.com",
             subject : "Verify your email for MystryMessage" ,
             react : VerificationEmail({username, otp : verifyCode})
         }) ;
