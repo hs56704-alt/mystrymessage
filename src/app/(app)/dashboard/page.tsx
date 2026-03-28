f208c7eeaa39d1fd31c405431a7617203d64e8a3
Himanshu Singh <hs56704@gmail.com>
2026-03-28 18:09:06 +0530

    28/03/2026 final commit

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,74 @@
 'use client';
 
+import { Message } from "@/model/User";
+import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useSession } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
 const page = () => {
+    const [ messages , setMessages] = useState<Message[]>([])
+    const [ isLoading , setIsLoading] = useState(false)
+    const [ isSwitchLoading , setIsSwitchLoading] = useState(false)
+
+    const handleDeleteMessage = (messageId : string) => {
+        setMessages(messages.filter((message) => message._id.toString() !== messageId))
+    }
+
+    const { data : session } = useSession()
+
+    const form = useForm({
+        resolver : zodResolver(acceptMessageSchema)
+    })
+
+    const { register , watch , setValue } = form;
+
+    const acceptMessages = watch('acceptMessages')
+
+    const fetchAcceptMessage = useCallback(async () => {
+        setIsSwitchLoading(true)
+        try {
+            const response = await axios.get<ApiResponse>('/api/accept-messages')
+            setValue('acceptMessages', response.data.isAcceptingMessage ?? false)
+        } catch (error) {
+            const axiosError = error as AxiosError<ApiResponse>;
+            toast.error(axiosError.response?.data.message || "Failed to fetch message settiings")
+        }finally{
+            setIsSwitchLoading(false)
+        }
+
+    }, [setValue])
+
+
+    const fetchMessages = useCallback( async (refresh : boolean = false) => {
+        setIsLoading(true)
+        setIsSwitchLoading(false)
+        try {
+            const response = await axios.get<ApiResponse>('/api/get-Messages')
+            setMessages(response.data.messages || [])
+            if(refresh){
+                toast.success("Showing latest Messages")
+            }
+        } catch (error) {
+            const axiosError = error as AxiosError<ApiResponse>;
+            toast.error(axiosError.response?.data.message || "Failed to fetch message settiings")
+        }finally{
+            setIsLoading(false)
+            setIsSwitchLoading(false)
+       
+
+            
+        }
+
+    } , [setIsLoading , setMessages])
+
+
+
+
     return(
         <div>Dashboard</div>
     )
--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -2,6 +2,6 @@ import { Message } from "../model/User";
 export interface ApiResponse{
     success : boolean ;
     message : string ;
-    isAcceptingMessages ?: boolean ;
-    messsages ?: Array<Message>;
+    isAcceptingMessage ?: boolean ;
+    messages ?: Array<Message>;
 }
