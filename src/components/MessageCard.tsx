25e000857441262605d6800139ff83208739aecd
Himanshu Singh <hs56704@gmail.com>
2026-03-28 13:54:30 +0530

    28/03/2026 02.00

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -14,19 +14,36 @@ import {
 } from "@/components/ui/alert"
 import { Button } from "./ui/button"
 import { AlertTriangle, X } from 'lucide-react'
+import { Message } from "@/model/User"
+import axios from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
+import { toast } from "sonner"
 
-const MessageCard = () => {
+type MessageCardProps = {
+    message : Message;
+    onMessageDelete : (messageId : string) => void 
+}
+
+
+const MessageCard = ({message , onMessageDelete} : MessageCardProps) => {
+
+    const handleDeleteConfirm = async () => {
+       const response = await axios.delete<ApiResponse>(`/api/delete-message/{message._id}`)
+       toast(response.data.message)
+
+       onMessageDelete(message._id.toString())
+    }
+
     return(
         <Card>
             <CardHeader>
                 <CardTitle>Card Title</CardTitle>
                 <Alert className="max-w-md border-amber-200 bg-amber-50 text-amber-900 dark:border-amber-900 dark:bg-amber-950 dark:text-amber-50">
                     <AlertTriangle />
-                    <AlertTitle>Your subscription will expire in 3 days.</AlertTitle>
-                    <Button variant='destructive'><X /></Button>
+                    <AlertTitle>Are you absolutely sure?</AlertTitle>
+                    <Button variant='destructive'><X className="w-5 h-5" /></Button>
                     <AlertDescription>
-                        Renew now to avoid service interruption or upgrade to a paid plan to
-                        continue using the service.
+                        
                     </AlertDescription>
                 </Alert>
                 <CardDescription>Card Description</CardDescription>
@@ -34,6 +51,8 @@ const MessageCard = () => {
             <CardContent>
             </CardContent>
             <CardFooter>
+                <Button>Cancel</Button>
+                <Button onClick={handleDeleteConfirm}>Continue</Button>
             </CardFooter>
         </Card>
     )
